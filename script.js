b623fa563b63efcdf71358e0dc61acf4c3b48f72
Ashley <alawrence22@ivytech.edu>
2026-02-22 23:52:05 -0500

    assignment

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
 // make event listener to trigger when the DOM is loaded (when visiting webpage)
-addEventListener("DOMContentLoaded", async function() {
-    const response = await fetch("http://localhost:3000/api/songs")
-    const songs = await response.json()
+addEventListener("DOMContentLoaded", function() {
+    loadCourses();
 
-    let html = ""
-    for (let song of songs) {
-        html += `<li>${song.title} - ${song.artist}</li>`
+        document.getElementById("courseForm").addEventListener("submit", async function(e) {
+        e.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const instructor = document.getElementById("instructor").value;
+
+        await fetch("http://localhost:3000/api/courses", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, instructor })
+        })
+
+        loadCourses();
+
+    });
+
+});
+
+async function loadCourses() {
+    const response = await fetch("http://localhost:3000/api/courses");
+    const courses = await response.json();
+
+    let html = "";
+
+    for (let course of courses) {
+        html += `
+        <li>
+        ${course.name} - ${course.instructor}
+        <button onclick="editCourse(${course.id}, '${course.name}', '${course.instructor}')">Edit</button>
+        <button onclick="deleteCourse(${course.id})">Delete</button>
+        </li>
+        `;
     }
+    document.querySelector("#courseList").innerHTML = html;
+}
 
-    document.querySelector("#addedSong").innerHTML = html
-})
+
+
+async function deleteCourse(id) {
+    await fetch(`http://localhost:3000/api/courses/${id}`, {
+        method: "DELETE"
+    });
+    loadCourses();
+}
+
+async function editCourse(id, currentName, currentInstructor) {
+    const newName = prompt("Enter new course name:", currentName);
+    const newInstructor = prompt("Enter new instructor:", currentInstructor);
+
+    if (!newName || !newInstructor) return;
+
+    await fetch(`http://localhost:3000/api/courses/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+        name: newName,
+        instructor: newInstructor
+        })
+    });
+    loadCourses();
+}
